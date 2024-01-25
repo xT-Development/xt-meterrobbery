1b0ff5d94a1534f4d993b7a127c45a4d2c0e3da8
xThrasherrr <thrashergamezz@gmail.com>
2024-01-24 20:57:37 -0700

    feat(ptfx): add coins ptfx

--- modules/client.lua
+++ modules/client.lua
@@ -9,8 +9,8 @@ function xTc.CreateMeters()
             distance = 2.0,
             items = Config.AcceptedItems,
             anyItem = true,
-            onSelect = function(data) 
-                TriggerEvent('xt-meterrobbery:client:RobMeter', data)
+            onSelect = function(data)
+                robParkingMeter(data)
             end,
         }
     })
@@ -18,6 +18,21 @@ end
 
 function xTc.RemoveMeters()
     exports.ox_target:removeModel(Config.Models, 'robMeters')
+end
+
+function xTc.PTFX(ENT)
+    local entCoords = GetEntityCoords(ENT)
+    local offset, rotation = vec3(entCoords.x, entCoords.y, entCoords.z), GetEntityRotation(ENT)
+    local dict, ptfx = 'core', 'ent_brk_coins'
+
+    lib.requestNamedPtfxAsset(dict, 1000)
+
+    UseParticleFxAsset(dict)
+
+    local startPTFX = StartParticleFxLoopedAtCoord(ptfx, offset.x, offset.y, (offset.z + 1), rotation.x, rotation.y, rotation.z, 1.0, false, false, false)
+
+    RemoveNamedPtfxAsset(dict)
+    StopParticleFxLooped(startPTFX, false)
 end
 
 return xTc
